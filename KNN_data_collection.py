3dc3863d5d1dbf3d56814089ace48ca628ae3a23
Eric Phillips <ericphillips99@me.com>
2020-12-03 18:35:02 -0800

    Added notes to KNN_data_collection

--- KNN_data_collection.py
+++ KNN_data_collection.py
@@ -1,24 +1,32 @@
-class KNN: 
-    def __init__(self, features, dummy_labels, k =3, model_type):
-        self.features = np.array(features)
-        self.labels = np.array(dummy_labels)
+import numpy as np
+class KNN:
+    def __init__(self,model_type,k=3):
         self.k = k
-        self.model_type = model_type
-        
-    def load_csv_file(path):
+        if model_type=='regressor' or model_type=='classifier':
+            self.model_type = model_type
+        else:
+            raise ValueError('Model type must be "regressor" or "classifier"')
+
+    def load_csv_file(self,path,response):
         data = []
         with open(path ,'r') as file:
             csv_reader = reader(file)
             for row in csv_reader:
                 data.append(row)
-        return np.array(data)
-    
-    def train_test_split(dataset, test_size = 0.3):
-                            dataset_ = np.array(dataset)
-                            np.randonm.shuffle(dataset_) # to avoid bias
-                            threshold = int(dataset_.shape[0] * 0.1)
-                            x_test = dataset_[:threshold, :-1]
-                            y_test = dataset_[:threshold, -1]
-                            x_train = dataset_[threshold:, :-1]
-                            y_train = dataset_[threshold:, -1]
-        return x_test, y_test, x_train, y_train
+        # Need to separate response variable from predictors (rest of the columns)
+        # Need to remove column names after this is done (remove first row)
+        self.x=np.array() # Predictor variables [store as instance attribute instead of returning]
+        self.y=np.array() # Response variables [store as instance attribute instead of returning]
+        print('Dataset successfully loaded.')
+
+    def train_test_split(self, test_size = 0.3):
+        # Need to select random rows from both x and y: possible solution could be to sample indexers from 0 to length of dataset without replacement using np.random.choice?
+            # Could select subset of x and y based on these random index vals (e.g., these vals become the training set, rest become test, etc)
+        dataset_ = np.array(dataset)
+        np.randonm.shuffle(dataset_)
+        threshold = int(dataset_.shape[0] * 0.1)
+        x_test = dataset_[:threshold, :-1]
+        y_test = dataset_[:threshold, -1]
+        x_train = dataset_[threshold:, :-1]
+        y_train = dataset_[threshold:, -1]
+        return x_test, y_test, x_train, y_train # Store as self.x_test, self.y_test, etc. instead of returning
