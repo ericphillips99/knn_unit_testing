261342cec997c74c864adf929445495e7a65ca51
Poojitha Gowthaman <pg.poojithagowthaman@gmail.com>
2020-12-09 00:08:25 -0600

    Data collection and metrics test cases

--- Unit_Testing/modelling/Data_collection/test_load_csv.py
+++ Unit_Testing/modelling/Data_collection/test_load_csv.py
@@ -1,7 +1,5 @@
 import unittest
 from KNN.modelling import KNN_data_collection as knn
-test_knn_regressor = knn.KNN("regressor",2)
-test_knn_classifier = knn.KNN("classifier",4)
 import numpy as np
         
 class Testload_csv(unittest.TestCase):
@@ -10,10 +8,10 @@ class Testload_csv(unittest.TestCase):
     assert_y_header = 0
     assert_x_header = 0
     assert_k = 0
-    x,y =0,0
+    x,y,test_knn_regressor =0,0, 0
     
     def setUpClass():
-        test_knn_regressor = knn.KNN("regressor",2)
+        Testload_csv.test_knn_regressor = knn.KNN("regressor",2)
     def setUp(self):
         self.data = np.arange(0,745).reshape(149,5)
         self.k = 2
@@ -22,10 +20,10 @@ class Testload_csv(unittest.TestCase):
         self.y = self.data[:,self.y_header] # counting the length of column header
         self.x = np.delete(self.data,self.y,axis=1).astype(float)
         
-        test_knn_regressor.load_csv('data_banknote_authentication.txt', '-0.44699' )
-        self.assert_y = test_knn_regressor.y
-        self.assert_x = test_knn_regressor.x
-        self.assert_k = test_knn_regressor.k
+        Testload_csv.test_knn_regressor.load_csv('datasets/data_banknote_authentication.txt', '-0.44699' )
+        self.assert_y = Testload_csv.test_knn_regressor.y
+        self.assert_x = Testload_csv.test_knn_regressor.x
+        self.assert_k = Testload_csv.test_knn_regressor.k
         
         #self.assertEqual(self.response,self.assert_response)
         self.assertEqual(np.size(self.y), np.size(self.assert_y))
@@ -39,10 +37,10 @@ class Testload_csv(unittest.TestCase):
     
     
 class TestVariables(unittest.TestCase):
-    x, y, assert_y, assert_x = 0, 0, 0, 0
+    x, y, assert_y, assert_x,test_knn_classifier = 0, 0, 0, 0, 0
     data = []
     def setUpClass():
-        test_knn_classifier = knn.KNN("classifier",4)
+        TestVariables.test_knn_classifier = knn.KNN("classifier",4)
     def setUp(self):
         self.data = np.arange(0,750).reshape(150,5)
         self.k = 4
@@ -52,10 +50,10 @@ class TestVariables(unittest.TestCase):
         self.y = self.data[:,self.y_header]
         self.x = (np.delete(self.data, self.y, axis = 1))
         
-        test_knn_classifier.load_csv('iris.csv', 'Species')
+        TestVariables.test_knn_classifier.load_csv('datasets/iris.csv', 'Species')
         
-        self.assert_y = test_knn_classifier.y
-        self.assert_x = test_knn_classifier.x
+        self.assert_y = TestVariables.test_knn_classifier.y
+        self.assert_x = TestVariables.test_knn_classifier.x
         
         self.assertIn(self.y_header, self.y)
         self.assertNotIn(self.x, self.y)
--- Unit_Testing/modelling/Data_collection/test_train_split.py
+++ Unit_Testing/modelling/Data_collection/test_train_split.py
@@ -1,8 +1,5 @@
-## REgressor
 import unittest
 from KNN.modelling import KNN_data_collection as knn
-test_knn_regressor = knn.KNN("regressor",2)
-test_knn_classifier = knn.KNN("classifier", 3)
 import numpy as np
 class TestDataCollection_regressor(unittest.TestCase):
     x_train = 0
@@ -13,10 +10,10 @@ class TestDataCollection_regressor(unittest.TestCase):
     assert_xtest = 0
     assert_ytrain = 0
     assert_ytest = 0
-    x =0
-    y =0
+    x, y =0, 0
+    test_knn_regressor =0
     def setUpClass():
-        test_knn_regressor = knn.KNN("regressor",2)
+        TestDataCollection_regressor.test_knn_regressor = knn.KNN("regressor",2)
         
         
     def setUp(self):
@@ -27,8 +24,8 @@ class TestDataCollection_regressor(unittest.TestCase):
         
     def test_train_test_split_regressor(self):
         
-        test_knn_regressor.x = self.x
-        test_knn_regressor.y = self.y
+        TestDataCollection_regressor.test_knn_regressor.x = self.x
+        TestDataCollection_regressor.test_knn_regressor.y = self.y
         
         self.x_train = int((1-self.test_size) * len(self.x))
         self.x_test = int(self.test_size * len(self.x))
@@ -36,11 +33,11 @@ class TestDataCollection_regressor(unittest.TestCase):
         self.y_test = int(self.test_size * len(self.y))
         
         
-        test_knn_regressor.train_test_split(0.25)
-        self.assert_xtrain = len(test_knn_regressor.x_train)
-        self.assert_xtest = len(test_knn_regressor.x_test)
-        self.assert_ytrain = len(test_knn_regressor.y_train)
-        self.assert_ytest = len(test_knn_regressor.y_test)
+        TestDataCollection_regressor.test_knn_regressor.train_test_split(0.25)
+        self.assert_xtrain = len(TestDataCollection_regressor.test_knn_regressor.x_train)
+        self.assert_xtest = len(TestDataCollection_regressor.test_knn_regressor.x_test)
+        self.assert_ytrain = len(TestDataCollection_regressor.test_knn_regressor.y_train)
+        self.assert_ytest = len(TestDataCollection_regressor.test_knn_regressor.y_test)
     
     
         if ((self.assertEqual(self.x_train,self.assert_xtrain)) and (self.assertEqual(self.x_test,self.assert_xtest)) and (self.assertEqual(self.y_train,self.assert_ytrain)) and (self.assertEqual(self.y_test,self.assert_ytest))):
@@ -63,10 +60,10 @@ class TestDataCollection_classifier(unittest.TestCase):
     assert_xtest = 0
     assert_ytrain = 0
     assert_ytest = 0
-    x =0
-    y =0
+    x, y = 0, 0
+    test_knn_classifier = 0
     def setUpClass():
-        test_knn_classifier = knn.KNN("classifier", 3)
+        TestDataCollection_classifier.test_knn_classifier = knn.KNN("classifier", 3)
     def setUp(self):
         self.test_size = 0.4
         self.x = np.array([[6.3,2.3,4.4,1.3],[6.8,3.2,5.9,2.3],[4.3,3,1.1,0.1],[3.5,3.1,1.0,0.5],[6.9,3.3,5.8,2.0]])
@@ -74,8 +71,8 @@ class TestDataCollection_classifier(unittest.TestCase):
         
     def test_train_test_split_classifier(self):
         
-        test_knn_classifier.x = self.x
-        test_knn_classifier.y = self.y
+        TestDataCollection_classifier.test_knn_classifier.x = self.x
+        TestDataCollection_classifier.test_knn_classifier.y = self.y
         
         self.x_train = int((1-self.test_size) * len(self.x))
         self.x_test = int(self.test_size * len(self.x))
@@ -84,11 +81,11 @@ class TestDataCollection_classifier(unittest.TestCase):
         
         
         
-        test_knn_classifier.train_test_split(0.4)
-        self.assert_xtrain = len(test_knn_classifier.x_train)
-        self.assert_xtest = len(test_knn_classifier.x_test)
-        self.assert_ytrain = len(test_knn_classifier.y_train)
-        self.assert_ytest = len(test_knn_classifier.y_test)
+        TestDataCollection_classifier.test_knn_classifier.train_test_split(0.4)
+        self.assert_xtrain = len(TestDataCollection_classifier.test_knn_classifier.x_train)
+        self.assert_xtest = len(TestDataCollection_classifier.test_knn_classifier.x_test)
+        self.assert_ytrain = len(TestDataCollection_classifier.test_knn_classifier.y_train)
+        self.assert_ytest = len(TestDataCollection_classifier.test_knn_classifier.y_test)
         
         
         
@@ -103,4 +100,4 @@ class TestDataCollection_classifier(unittest.TestCase):
     def tearDownClass():
         print('Result successfull')
 
-unittest.main (argv =[''], verbosity=2, exit= False)      
+unittest.main (argv =[''], verbosity=2, exit= False)  
